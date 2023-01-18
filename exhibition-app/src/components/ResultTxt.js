79b2371e8f0a2eb272d35eb9a19c5df02b82452a
cupmilk <105467771+cupmilk@users.noreply.github.com>
2023-01-18 22:15:03 +0900

    updated: api error fix

--- exhibition-app/src/components/ResultTxt.js
+++ exhibition-app/src/components/ResultTxt.js
@@ -1,6 +1,5 @@
 import React, { useCallback, useEffect, useState } from "react";
 import styled from "styled-components";
-import theme from "./../styles/theme";
 import { css } from "styled-components";
 import resultCategoryInfo from "../utils/resultCategoryInfo";
 import ErrorPage from "../pages/ErrorPage";
